712d1c3c568ab2113e890d6c04ff5530fed92608
Denis Yakorev <denisyakorev@mail.ru>
2018-03-25 12:20:12 +0300

    add telegramm send message

--- bot/views.py
+++ bot/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+import requests
 
 
 
@@ -13,13 +14,27 @@ def hello_world(request):
     return response
 
 @csrf_exempt
-def dispatcher(request):
-    print ('POST: ')
-    print (request.POST)
-    update = request.POST.get('update', 'empty')
-    print(update)
-    
+def dispatcher(request):    
+    update = json.loads(request.body.decode('utf-8'))
+    chat_id = update.message.chat.id
+    send_message(chat_id)
     response = HttpResponse(json.dumps({'message': 'ок'}),
                              content_type='application/json')
     response.status_code = 200
     return response
+
+
+def send_message(chat_id):
+    #url = 'https://api.telegram.org/bot565321270:AAGVaNTz5a2pscli1_VnG0vh2Fv0CarejLM/sendMessage'
+    data = {
+        'chat_id':chat_id,
+        'text': 'hello world'
+    }
+
+    r = requests.post(url, data=data)
+    print (r.status_code)
+
+    if r.status_code == 200:
+        return True
+    else:
+        return False

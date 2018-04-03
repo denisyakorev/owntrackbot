50696b4422825510e9d7e25d16ad415babc96579
linux17 <denisyakorev@mail.ru>
2018-04-03 09:08:38 -0700

    make some test cases

--- bot/tests.py
+++ bot/tests.py
@@ -6,65 +6,75 @@ class MessageAnalysysTest(TestCase):
 	def setUp(self):
 		self.bot = Bot.objects.get(messager='telegram')
 
+
+	def tearDown(self):
+		pass
+
 	def check_out_message(self):
 		"""Проверяет правильность анализа сообщений"""
 		equals = [
 			['?', 
 				[
-					['get_info', 'user_profile'],
+					['read', 'user_profile'],
 				],
 			],
 
 			['? #unrealtask', 
 				[
-					['get_info', 'error_task_does_not_exist'],
+					['read', 'error_task_does_not_exist'],
 				]
 			],
 
 			['? #realtask',
 				[
-					['get_info', 'task_id']
+					['read', 'task_id']
+				]
+			],
+
+			['? #realtaskWithTwinInOtherGroup',
+				[
+					['read', 'error_too_more_objects']
 				]
 			],
 
 			['?#realtask',
 				[
-					['get_info', 'task_id']
+					['read', 'task_id']
 				]
 
 			],
 
 			['? @realgroup',
 				[
-					['get_info', 'group_id']
+					['read', 'group_id']
 				]
 
 			],
 
 			['? @unrealgroup',
 				[
-					['get_info', 'error_group_does_not_exist']
+					['read', 'error_group_does_not_exist']
 				]
 
 			],
 
 			['? *realcategory',
 				[
-					['get_info', 'category_id']
+					['read', 'category_id']
 				]
 
 			],
 
 			['? *unrealcategory',
 				[
-					['get_info', 'error_category_does_not_exist']
+					['read', 'error_category_does_not_exist']
 				]
 
 			],
 
 			['+ #newtask',
 				[
-					['create', 'task_newtask']
+					['create', 'task_newtask in group default in category default']
 				]
 
 			],
@@ -77,13 +87,161 @@ class MessageAnalysysTest(TestCase):
 
 			['+ #newtask @existinggroup',
 				[
-					['get', 'error_task_exists']
+					['create', 'task_newtask in group existinggroup in category default']
+				]
+			],
+
+			['+ #newtask @existinggroup *existingcategory',
+				[
+					['create', 'task_newtask in group existinggroup in category existingcategory']
+				]
+			],
+
+			['+ #newtask @newgroup *existingcategory',
+				[
+					['create', 'error_group_does_not_exists']
+				]
+			],
+
+			['+ #newtask @existinggroup *newcategory',
+				[
+					['create', 'error_category_does_not_exists']
+				]
+			],
+
+			['+ #newtask #newtask2 @existinggroup *existingcategory',
+				[
+					['create', 'error_too_many_tasks']
+				]
+			],
+
+			['+ #newtask @existinggroup @existinggroup1 *existingcategory',
+				[
+					['create', 'error_too_many_groups']
+				]
+			],
+
+			['+ #newtask @existinggroup *existingcategory *existingcategory1',
+				[
+					['create', 'error_too_many_categories']
+				]
+			],
+
+			['+ @existinggroup *existingcategory',
+				[
+					['create', 'error_group_exists']
+				]
+			],
+
+			['+ @newgroup *existingcategory',
+				[
+					['create', 'group_newgroup in category existingcategory']
+				]
+			],
+
+			['+ @existinggroup #newtask *existingcategory',
+				[
+					['create', 'task_newtask in group existinggroup in category existingcategory']
+				]
+			],
+
+			['+ @existinggroup *existingcategory  #newtask',
+				[
+					['create', 'task_newtask in group existinggroup in category existingcategory']
+				]
+			],
+
+			['+ *existingcategory @existinggroup  #newtask',
+				[
+					['create', 'task_newtask in group existinggroup in category existingcategory']
+				]
+			],
+
+			['+ *existingcategory @existinggroup',
+				[
+					['create', 'error_group_exists']
+				]
+			],
+
+			['+ *newcategory @existinggroup',
+				[
+					['create', 'error_category_does_not_exists']
+				]
+			],
+
+			['+ *newcategory',
+				[
+					['create', 'category_newcategory']
+				]
+			],
+
+			['+ *existingcategory',
+				[
+					['create', 'error_category_exists']
+				]
+			],
+
+			['1h23m #task1',
+				[
+					['update', 'add 83 min to task1']
+				]
+			],
+
+			['1h23m #taskWithTwinInOtherGroup',
+				[
+					['update', 'error_too_many_tasks']
+				]
+			],
+
+			['+1h23m #newtask',
+				[
+					['update', 'error_task_does_not_exists']
+				]
+			],
+
+			['+1h23m #existingtask',
+				[
+					['update', 'error_task_does_not_exists']
+				]
+			],
+
+			['?1h23m #existingtask',
+				[
+					['read', 'task_existingtask']
+				]
+			],
+
+			['23m #existingtask',
+				[
+					['read', 'add 23 min to task1']
+				]
+			],
+
+			['23 #existingtask',
+				[
+					['read', 'error_unknown_command']
+				]
+			],
+
+			['m #existingtask',
+				[
+					['read', 'error_unknown_command']
+				]
+			],
+
+			['! #existingtask',
+				[
+					['finish', 'task_existingtask']
 				]
 			]
 
 
+
 		]
 
-		self.assertEqual(bot.analyze_message())
-		pass
+		for elem in equals:
+			self.assertEqual(bot.analyze_message(equals[elem][0]), equals[elem][1])
+		
+
+
 

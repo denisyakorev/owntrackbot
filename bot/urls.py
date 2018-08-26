625c4138834c85cc4bad6abcd1a88c073861948e
Denis Yakorev <denisyakorev@mail.ru>
2018-08-26 19:11:54 +0300

    add api for profile info

--- bot/urls.py
+++ bot/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from bot import views
+from core import views as core_views
 from django.contrib import admin
 
 urlpatterns = [
     path('', views.dispatcher),
+    path('profile_info/<str:profile_id>/', core_views.get_profile_info, name='profile info'),
 ]

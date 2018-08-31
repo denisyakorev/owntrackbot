f1bb865992b2c3d0d2b01fe17ea67c41f5d23d3b
Denis Yakorev <denisyakorev@mail.ru>
2018-08-31 20:25:54 +0300

    add staticroot

--- owntrackbot/settings.py
+++ owntrackbot/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 TELEGRAM_TOKEN = '565321270:AAGVaNTz5a2pscli1_VnG0vh2Fv0CarejLM'
 
